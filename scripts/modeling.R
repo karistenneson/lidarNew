768b67f8f51d6449f47e603e1b77fca1e8684ce2
MATTHEW S. PATTERSON <maspatte@uw.edu>
2017-10-20 19:48:45 -0700

    Developed final models.

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -33,20 +33,6 @@ summary(BioMass.Mod)
 BioMass.Mod$namesx[which(BioMass.Mod$probne0>0.5)][-1]
 BioMass.Modc$namesx[which(BioMass.Modc$probne0>0.5)][-1]
 
-# Correlation in variables in the median model
-MedModVar <- select(DATA.mod, BioMass.Mod$namesx[which(BioMass.Mod$probne0>0.5)][-1])
-
-corrgram(MedModVar, order=T, lower.panel=panel.ellipse,
-         upper.panel=panel.cor, text.panel=panel.txt,
-         main="Lidar Predictor Data in PC2/PC1 Order")
-
-#Fairly strong correlations between many variables, several clear pairs that should probably be removed.
-# Variable marked with * seems best to remove
-# Total_all_returns & Total_first_returns*
-# Elev_CV* & Elev_LCV
-# Pct_all_returns_above...* & Pct_first_returns_above...
-# All_returns_above_mean_div_Total_first_returns_x_100 & Pct_first_returns_above_mean*
-
 # Model diagnostics
 plot(BioMass.Mod, ask=F)
 plot(BioMass.Modc, ask=F)
@@ -57,10 +43,10 @@ plot(BioMass.Modc, ask=F)
 HPM <- predict(BioMass.Mod, estimator="HPM")
 BioMass.Mod$namesx[HPM$bestmodel+1][-1]
 
-HPMlm <- lm(log(STBIOMS)~ Elev_skewness + Elev_P80 + Pct_first_returns_above_ht + Pct_all_returns_above_ht +
+HPMlm <- lm(log(STBIOMS)~ Elev_CV + Elev_LCV + Elev_P80 + Pct_first_returns_above_ht + Pct_all_returns_above_ht +
               all_returns_above_ht_div_Total_first_returns_x_100 + Pct_first_returns_above_mean +
               All_returns_above_mean_div_Total_first_returns_x_100 + Total_first_returns +
-              Total_all_returns + R3ERUCODE, data=DATA.mod)
+              Total_all_returns + R3ERUCODE + PlotSizeAcres, data=DATA.mod)
 
 summary(HPMlm)
 plot(HPMlm, ask=F)
@@ -70,9 +56,9 @@ MPM <- predict(BioMass.Mod, estimator="MPM")
 BioMass.Mod$namesx[MPM$bestmodel+1][-1]
 
 MPMlm <- lm(log(STBIOMS)~ Elev_CV + Elev_LCV + Elev_P80+ Pct_first_returns_above_ht + 
-              Pct_all_returns_above_ht + Pct_first_returns_above_mean + 
+              Pct_all_returns_above_ht + All_returns_above_ht + Pct_first_returns_above_mean + 
               All_returns_above_mean_div_Total_first_returns_x_100 + Total_first_returns + 
-              Total_all_returns + elevation + NDVI_Amp, data=DATA.mod)
+              Total_all_returns + R3ERUCODE + PlotSizeAcres, data=DATA.mod)
 
 summary(MPMlm)
 plot(MPMlm, ask=F) # This model is pretty solid, better redisdual distribution than HPM, and better adjusted R^2 
@@ -82,13 +68,73 @@ plot(MPMlm, ask=F) # This model is pretty solid, better redisdual distribution t
 BPM <- predict(BioMass.Mod, estimator="BPM") #not running, grrrr
 
 #A possible final model? 
-FinModVar <- select(DATA.mod, Elev_LCV, Elev_P80, Pct_all_returns_above_ht, Total_all_returns,
-                    elevation, NDVI_Amp)
+FinModVar <- select(DATA.mod, Elev_LCV, Elev_P80, Pct_first_returns_above_ht,
+                    All_returns_above_ht, R3ERUCODE, PlotSizeAcres)
+
 corrgram(FinModVar, order=T, lower.panel=panel.ellipse,
          upper.panel=panel.cor, text.panel=panel.txt,
          main="Lidar Predictor Data in PC2/PC1 Order") # No correlations above ~0.5
 
-BioMass.ModelF <- lm(log(STBIOMS)~ Elev_LCV + Elev_P80+ Pct_all_returns_above_ht + 
-                       Total_all_returns + elevation + NDVI_Amp, data=DATA.mod)
+BioMass.ModelF <- lm(log(STBIOMS)~ Elev_LCV + Elev_P80 + Pct_first_returns_above_ht +
+                       All_returns_above_ht + R3ERUCODE + PlotSizeAcres, data=DATA.mod)
 summary(BioMass.ModelF)
 plot(BioMass.ModelF) # Some slight non-linearities in residuals and QQ. 
+
+
+### Same business, but for Total Cubit Feet of wood.
+
+# Full variable pool, truncated poisson prior, hyper-g
+TCUFT.Mod <- bas.lm(log(TCUFT)~ . -STBIOMS, 
+                      data=DATA.mod, 
+                      prior="hyper-g",
+                      alpha = 3,
+                      modelprior=tr.poisson(10,30),
+                      method="MCMC+BAS")
+
+summary(TCUFT.Mod)
+
+# What are the median model variables
+TCUFT.Mod$namesx[which(TCUFT.Mod$probne0>0.5)][-1]
+
+# Highest Probability Model
+HPM <- predict(TCUFT.Mod, estimator="HPM")
+TCUFT.Mod$namesx[HPM$bestmodel+1][-1]
+
+HPMlm <- lm(log(TCUFT)~ Elev_skewness + Elev_P80 + Pct_first_returns_above_ht + Pct_all_returns_above_ht +
+            All_returns_above_ht + Pct_first_returns_above_mean + All_returns_above_mean_div_Total_first_returns_x_100 + 
+              Total_first_returns + Total_all_returns + R3ERUCODE + elevation + slope, data=DATA.mod)
+
+summary(HPMlm)
+plot(HPMlm, ask=F)
+
+# Median Probability Model 
+MPM <- predict(TCUFT.Mod, estimator="MPM")
+TCUFT.Mod$namesx[MPM$bestmodel+1][-1]
+# Same as HPM!
+
+# Best Predictive Model, closest to BMA in terms of squared error loss, takes a pretty long time to find. 
+
+BPM <- predict(BioMass.Mod, estimator="BPM") #not running, grrrr
+
+#A possible final model? 
+FinModVar <- select(DATA.mod, Elev_skewness, Elev_P80, Pct_all_returns_above_ht,
+                    Total_first_returns, R3ERUCODE, elevation, slope)
+
+corrgram(FinModVar, order=T, lower.panel=panel.ellipse,
+         upper.panel=panel.cor, text.panel=panel.txt,
+         main="Lidar Predictor Data in PC2/PC1 Order") # No correlations above ~0.5
+
+TCUFT.ModelF <- lm(log(TCUFT)~ Elev_skewness + Elev_P80 + Pct_all_returns_above_ht +
+                   Total_first_returns + R3ERUCODE + elevation + slope, data=DATA.mod)
+summary(TCUFT.ModelF)
+plot(TCUFT.ModelF) # Some slight non-linearities in residuals and QQ. 
+
+### Combined final model?
+
+FinModB <- lm(log(STBIOMS)~ Elev_LCV + Elev_skewness + Elev_P80 + Pct_all_returns_above_ht +
+               Total_first_returns + R3ERUCODE + elevation + slope + PlotSizeAcres, data=DATA.mod)
+summary(FinModB)
+
+FinModT <- lm(log(TCUFT)~ Elev_LCV + Elev_skewness + Elev_P80 + Pct_all_returns_above_ht +
+                Total_first_returns + R3ERUCODE + elevation + slope + PlotSizeAcres, data=DATA.mod)
+summary(FinModT)
