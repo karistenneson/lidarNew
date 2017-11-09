07b768e79eb3e66be28a19b8cddc1d652b87a9c1
MATTHEW S. PATTERSON <maspatte@uw.edu>
2017-11-09 14:28:35 -0800

    Streamlined slightly, added forest as variable, did run removing outliers.

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -10,7 +10,7 @@ source(file="DataPrep.R")
 ### Load required packages
 library(BAS)
 library(corrgram)
-library(robustbase)
+#library(robustbase)
 
 # Full variable pool, centered, truncated poisson prior, hyper-g
 BioMass.Modc <- bas.lm(log(STBIOMS)~ . -TCUFT,
@@ -46,10 +46,10 @@ plot(BioMass.Modc, ask=F)
 HPM <- predict(BioMass.Mod, estimator="HPM")
 BioMass.Mod$namesx[HPM$bestmodel+1][-1]
 
-HPMlm <- lm(log(STBIOMS)~ Elev_CV + Elev_LCV + Elev_P80 + Pct_first_returns_above_ht + Pct_all_returns_above_ht +
-              all_returns_above_ht_div_Total_first_returns_x_100 + Pct_first_returns_above_mean +
-              All_returns_above_mean_div_Total_first_returns_x_100 + Total_first_returns +
-              Total_all_returns + R3ERUCODE + PlotSizeAcres, data=DATA.mod)
+HPMlm <- lm(log(STBIOMS)~ Elev_ave + Elev_CV + Elev_LCV + Pct_first_returns_above_ht + 
+              Pct_all_returns_above_ht + all_returns_above_ht_div_Total_first_returns_x_100 + 
+              Pct_first_returns_above_mean + All_returns_above_mean_div_Total_first_returns_x_100 + 
+              Total_first_returns + Total_all_returns + R3ERUCODE + Forest + PlotSizeAcres, data=DATA.mod)
 
 summary(HPMlm)
 plot(HPMlm, ask=F)
@@ -61,7 +61,7 @@ BioMass.Mod$namesx[MPM$bestmodel+1][-1]
 MPMlm <- lm(log(STBIOMS)~ Elev_CV + Elev_LCV + Elev_P80+ Pct_first_returns_above_ht + 
               Pct_all_returns_above_ht + All_returns_above_ht + Pct_first_returns_above_mean + 
               All_returns_above_mean_div_Total_first_returns_x_100 + Total_first_returns + 
-              Total_all_returns + R3ERUCODE + PlotSizeAcres, data=DATA.mod)
+              Total_all_returns + R3ERUCODE + Forest + PlotSizeAcres, data=DATA.mod)
 
 summary(MPMlm)
 plot(MPMlm, ask=F) # This model is pretty solid, better redisdual distribution than HPM, and better adjusted R^2 
@@ -69,20 +69,6 @@ plot(MPMlm, ask=F) # This model is pretty solid, better redisdual distribution t
 # Best Predictive Model, closest to BMA in terms of squared error loss, takes a pretty long time to find. 
 
 BPM <- predict(BioMass.Mod, estimator="BPM") #not running, grrrr
-
-#A possible final model? 
-FinModVar <- select(DATA.mod, Elev_LCV, Elev_P80, Pct_first_returns_above_ht,
-                    All_returns_above_ht, R3ERUCODE, PlotSizeAcres)
-
-corrgram(FinModVar, order=T, lower.panel=panel.ellipse,
-         upper.panel=panel.cor, text.panel=panel.txt,
-         main="Lidar Predictor Data in PC2/PC1 Order") # No correlations above ~0.5
-
-BioMass.ModelF <- lm(log(STBIOMS)~ Elev_LCV + Elev_P80 + Pct_first_returns_above_ht +
-                       All_returns_above_ht + R3ERUCODE + PlotSizeAcres, data=DATA.mod)
-summary(BioMass.ModelF)
-plot(BioMass.ModelF) # Some slight non-linearities in residuals and QQ. 
-
 
 ### Same business, but for Total Cubit Feet of wood.
 
@@ -113,42 +99,72 @@ plot(HPMlm, ask=F)
 # Median Probability Model 
 MPM <- predict(TCUFT.Mod, estimator="MPM")
 TCUFT.Mod$namesx[MPM$bestmodel+1][-1]
-# Same as HPM!
+
+MPMlm <- lm(log(TCUFT)~ Elev_skewness + Elev_P80 + Pct_first_returns_above_ht + Pct_all_returns_above_ht +
+              All_returns_above_ht + Pct_first_returns_above_mean + All_returns_above_mean_div_Total_first_returns_x_100 + 
+              Total_first_returns + Total_all_returns + R3ERUCODE + Forest + elevation + slope, data=DATA.mod)
+
+summary(MPMlm)
+plot(MPMlm, ask=F)
 
 # Best Predictive Model, closest to BMA in terms of squared error loss, takes a pretty long time to find. 
 
 BPM <- predict(BioMass.Mod, estimator="BPM") #not running, grrrr
 
-#A possible final model? 
-FinModVar <- select(DATA.mod, Elev_skewness, Elev_P80, Pct_all_returns_above_ht,
-                    Total_first_returns, R3ERUCODE, elevation, slope)
-
-corrgram(FinModVar, order=T, lower.panel=panel.ellipse,
-         upper.panel=panel.cor, text.panel=panel.txt,
-         main="Lidar Predictor Data in PC2/PC1 Order") # No correlations above ~0.5
-
-TCUFT.ModelF <- lm(log(TCUFT)~ Elev_skewness + Elev_P80 + Pct_all_returns_above_ht +
-                   Total_first_returns + R3ERUCODE + elevation + slope, data=DATA.mod)
-summary(TCUFT.ModelF)
-plot(TCUFT.ModelF) # Some slight non-linearities in residuals and QQ. 
-
 ### Combined final model?
 cor.test(DATA.mod$STBIOMS, DATA.mod$TCUFT)
 # Standing biomass and volume are 0.98 correlated. 
 
-FinModB <- lm(log(STBIOMS)~ Elev_LCV + Elev_skewness + Elev_P80 + Pct_all_returns_above_ht +
-               Total_first_returns + R3ERUCODE + elevation + slope + PlotSizeAcres, data=DATA.mod)
+FinModVarFull <- select(DATA.mod, Elev_skewness, Elev_CV, Elev_LCV, Elev_P80, Pct_first_returns_above_ht, 
+                    Pct_all_returns_above_ht, All_returns_above_ht, Pct_first_returns_above_mean, 
+                    All_returns_above_mean_div_Total_first_returns_x_100, Total_first_returns,
+                    Total_all_returns, R3ERUCODE, elevation, slope, PlotSizeAcres, Forest)
+
+corrgram(FinModVarFull, order=T, lower.panel=panel.ellipse,
+         upper.panel=panel.cor, text.panel=panel.txt,
+         main="Lidar Predictor Data in PC2/PC1 Order") # Trim until no correlations above ~0.5
+
+FinModVarTrim <- select(DATA.mod, Elev_skewness, Elev_LCV, Elev_P80, Total_first_returns, 
+                        Pct_first_returns_above_ht, R3ERUCODE, elevation, slope, PlotSizeAcres, Forest)
+
+corrgram(FinModVarTrim, order=T, lower.panel=panel.ellipse,
+         upper.panel=panel.cor, text.panel=panel.txt,
+         main="Lidar Predictor Data in PC2/PC1 Order") # Trim until no correlations above ~0.5
+
+# Final Model Performance
+
+FinModB <- lm(log(STBIOMS)~ Elev_skewness + Elev_LCV + Elev_P80 + Total_first_returns + 
+                Pct_all_returns_above_ht + elevation + slope + PlotSizeAcres + Forest + R3ERUCODE , 
+              data=DATA.mod)
 summary(FinModB)
 plot(FinModB, ask=F)
 
-FinModT <- lm(log(TCUFT)~ Elev_LCV + Elev_skewness + Elev_P80 + Pct_all_returns_above_ht +
-                Total_first_returns + R3ERUCODE + elevation + slope + PlotSizeAcres, data=DATA.mod)
+FinModT <- lm(log(TCUFT)~ Elev_skewness + Elev_LCV + Elev_P80 + Total_first_returns + 
+                Pct_all_returns_above_ht + elevation + slope + PlotSizeAcres + Forest + R3ERUCODE , 
+              data=DATA.mod)
 summary(FinModT)
 plot(FinModT, ask=F)
 
 # Possible Outliers
 Outliers <- c("53","1148","1238","1242","2145", "2651")
+Outliers <- which(rownames(DATA.mod) %in% Outliers)
 DATA.mod[Outliers, c(1,2)] #most low values, though 1238 has a fair bit of Biomass. 
+DATA.mod.t <- DATA.mod[-Outliers,]
+
+# Final Model Performance
+
+FinModBt <- lm(log(STBIOMS)~ Elev_skewness + Elev_LCV + Elev_P80 + Total_first_returns + 
+                Pct_all_returns_above_ht + elevation + slope + PlotSizeAcres + Forest + R3ERUCODE , 
+              data=DATA.mod.t)
+summary(FinModBt)
+plot(FinModBt, ask=F)
+
+FinModTt <- lm(log(TCUFT)~ Elev_skewness + Elev_LCV + Elev_P80 + Total_first_returns + 
+                Pct_all_returns_above_ht + elevation + slope + PlotSizeAcres + Forest + R3ERUCODE , 
+              data=DATA.mod.t)
+summary(FinModTt)
+plot(FinModTt, ask=F)
+
 
 # ### RObust versions of final model.
 # 
